dd7b4ea9b51dfb05e728f97a5234a91dd872e794
Jorge Nerin <jnerin@gmail.com>
2015-03-18 15:56:01 +0100

    First steps to work with both apt & yum

--- user-data.sh
+++ user-data.sh
@@ -2,8 +2,14 @@
 # The idea is that the user-data of the AutoScalingGroup is something like:
 # curl -s https://github.com/jnerin/varnish-4-aws-ec2-autoscalinggroup/raw/master/user-data.sh | bash -s --
 
-apt-get install apt-transport-https awscli jq
+APT=$(lsb_release --id --short |egrep -qi "\b(debian|ubuntu)\b" ; echo $?)
+YUM=$(lsb_release --id --short |egrep -qi "\b(redhat|fedora|centos)\b" ; echo $?)
 
+if [ $APT -eq 0 ] ; then
+	apt-get install apt-transport-https awscli jq
+elif [ $YUM -eq 0 ] ; then
+	yum install awscli jq
+fi
 AWS_REGION="eu-west-1"
 
 mkdir -p $HOME/.aws
@@ -20,12 +26,19 @@ EOF
 BACKEND_LAYER="$(aws ec2 describe-tags  --filters "Name=resource-id,Values=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)" "Name=key,Values=backend-layer" | jq -r ".Tags[0].Value")"
 
 
-curl https://repo.varnish-cache.org/GPG-key.txt | apt-key add -
-echo "deb https://repo.varnish-cache.org/ubuntu/ precise varnish-4.0" >> /etc/apt/sources.list.d/varnish-cache.list
-apt-get update
-apt-get -y install varnish 
 
-curl -o /etc/varnish/autoscalinggroup.vcl https://github.com/jnerin/varnish-4-aws-ec2-autoscalinggroup/raw/master/varnish/autoscalinggroup.vcl
+if [ $APT -eq 0 ] ; then
+	curl https://repo.varnish-cache.org/GPG-key.txt | apt-key add -
+	echo "deb https://repo.varnish-cache.org/ubuntu/ precise varnish-4.0" >> /etc/apt/sources.list.d/varnish-cache.list
+	apt-get update
+	apt-get -y install varnish 
+elif [ $YUM -eq 0 ] ; then
+	rpm --nosignature -i https://repo.varnish-cache.org/redhat/varnish-4.0.el6.rpm
+	yum install varnish
+fi
+
+
+curl -o /etc/varnish/autoscalinggroup.vcl https://raw.githubusercontent.com/jnerin/varnish-4-aws-ec2-autoscalinggroup/master/varnish/autoscalinggroup.vcl
 
 # Change the varnish boot vcl
 sed -ri.orig -e 's/^([^#].*-f \/etc\/varnish\/).*\.vcl/\1autoscalinggroup.vcl/' /etc/defaults/varnish
